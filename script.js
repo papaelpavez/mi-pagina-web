00f3a59fbc1870aaaf53414597e323d8a6f08e03
papaelpavez <papaelpavez@gmail.com>
2024-12-04 01:21:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const menuNav = document.getElementById('menu-nav');
+    menuNav.classList.toggle('menu-nav-active');
+});
